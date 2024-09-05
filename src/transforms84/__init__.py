ba80e119ea214875b6ac7b1b65b2855acba1f6ee
Daniel Stoops <danielst@omnisys.co.il>
2024-09-05 12:42:25 +0300

    Support py38 and py39

--- src/transforms84/__init__.py
+++ src/transforms84/__init__.py
@@ -1,4 +1,6 @@
+from typing import Union
+
 import numpy as np
 
-DTYPES_SUPPORTED = np.float32 | np.float64 | np.int8 | np.int16 | np.int32 | np.int64
+DTYPES_SUPPORTED = Union[np.float32, np.float64, np.int8, np.int16, np.int32, np.int64]
 __version__ = "0.2.0"
