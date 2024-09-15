c66805769268462f3fa91bc957548e0bade47e44
Daniel Stoops <dainelst@omnisys.co.il>
2024-09-15 21:33:53 +0300

    Update include_dirs in setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,29 @@
+import os
+
 import numpy as np
 from setuptools import Extension, setup
+
+include_dirs = [
+    np.get_include(),
+    "\\".join((os.path.dirname(os.path.realpath(__file__)), "include")),
+]
 
 setup(
     ext_modules=[
         Extension(
             "transforms84.transforms",
             sources=["include/transforms.c"],
-            include_dirs=[np.get_include(), "include/"],
+            include_dirs=include_dirs,
         ),
         Extension(
             "transforms84.distances",
             sources=["include/distances.c"],
-            include_dirs=[np.get_include(), "include/"],
+            include_dirs=include_dirs,
         ),
         Extension(
             "transforms84.helpers",
             sources=["include/helpers.c"],
-            include_dirs=[np.get_include(), "include/"],
+            include_dirs=include_dirs,
         ),
     ],
 )
