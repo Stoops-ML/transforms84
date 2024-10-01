258b8f7b747a2ad0a0e81d1cfc0cbc2553425d48
Daniel Stoops <dainelst@omnisys.co.il>
2024-10-01 09:05:01 +0300

    Bump to 0.3.0

--- src/transforms84/__init__.py
+++ src/transforms84/__init__.py
@@ -3,4 +3,4 @@ import os
 commit_hash = os.getenv("COMMIT_HASH", "")
 if commit_hash:  # pragma: no cover
     commit_hash = f"+{commit_hash}"
-__version__ = f"0.2.0{commit_hash}"
+__version__ = f"0.3.0{commit_hash}"
