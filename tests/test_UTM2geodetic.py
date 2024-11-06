cd68c4372ad2bf1a891cc00e5f179a8facf41b58
Daniel Stoops <dainelst@omnisys.co.il>
2024-11-06 12:05:18 +0200

    test1

--- tests/test_UTM2geodetic.py
+++ tests/test_UTM2geodetic.py
@@ -27,49 +27,49 @@ def test_point_int(dtype):
     assert np.isclose(out[1, 0], np.deg2rad(34.999995))
 
 
-@pytest.mark.parametrize("dtype", [np.int64, np.int32])
-def test_points_int(dtype):
-    in_arr = np.array(
-        [
-            [[690950.0], [3431318.0]],
-            [[690950.0], [3431318.0]],
-        ],
-        dtype=dtype,
-    )
-    out = UTM2geodetic(in_arr, 36, "R", WGS84.a, WGS84.b)
-    assert np.all(np.isclose(out[:, 0, 0], np.deg2rad(30.999992)))
-    assert np.all(np.isclose(out[:, 1, 0], np.deg2rad(34.999995)))
+# @pytest.mark.parametrize("dtype", [np.int64, np.int32])
+# def test_points_int(dtype):
+#     in_arr = np.array(
+#         [
+#             [[690950.0], [3431318.0]],
+#             [[690950.0], [3431318.0]],
+#         ],
+#         dtype=dtype,
+#     )
+#     out = UTM2geodetic(in_arr, 36, "R", WGS84.a, WGS84.b)
+#     assert np.all(np.isclose(out[:, 0, 0], np.deg2rad(30.999992)))
+#     assert np.all(np.isclose(out[:, 1, 0], np.deg2rad(34.999995)))
 
 
-@pytest.mark.parametrize("dtype", [np.float64, np.float32])
-def test_point(dtype):
-    in_arr = np.array([[690950.46], [3431318.84]], dtype=dtype)
-    out = UTM2geodetic(in_arr, 36, "R", WGS84.a, WGS84.b)
-    assert np.all(np.isclose(out[0, 0], np.deg2rad(31.0)))
-    assert np.all(np.isclose(out[1, 0], np.deg2rad(35.0)))
+# @pytest.mark.parametrize("dtype", [np.float64, np.float32])
+# def test_point(dtype):
+#     in_arr = np.array([[690950.46], [3431318.84]], dtype=dtype)
+#     out = UTM2geodetic(in_arr, 36, "R", WGS84.a, WGS84.b)
+#     assert np.all(np.isclose(out[0, 0], np.deg2rad(31.0)))
+#     assert np.all(np.isclose(out[1, 0], np.deg2rad(35.0)))
 
 
-@pytest.mark.parametrize("dtype", [np.float64, np.float32])
-def test_points(dtype):
-    in_arr = np.array(
-        [
-            [[690950.46], [3431318.84]],
-            [[690950.46], [3431318.84]],
-        ],
-        dtype=dtype,
-    )
-    out = UTM2geodetic(in_arr, 36, "R", WGS84.a, WGS84.b)
-    assert np.all(np.isclose(out[:, 0, 0], np.deg2rad(31.0)))
-    assert np.all(np.isclose(out[:, 1, 0], np.deg2rad(35.0)))
+# @pytest.mark.parametrize("dtype", [np.float64, np.float32])
+# def test_points(dtype):
+#     in_arr = np.array(
+#         [
+#             [[690950.46], [3431318.84]],
+#             [[690950.46], [3431318.84]],
+#         ],
+#         dtype=dtype,
+#     )
+#     out = UTM2geodetic(in_arr, 36, "R", WGS84.a, WGS84.b)
+#     assert np.all(np.isclose(out[:, 0, 0], np.deg2rad(31.0)))
+#     assert np.all(np.isclose(out[:, 1, 0], np.deg2rad(35.0)))
 
 
-@pytest.mark.parametrize("dtype", [np.float64, np.float32])
-def test_parallel(dtype):
-    in_arr = np.ascontiguousarray(
-        np.tile(np.array([[690950.46], [3431318.84]], dtype=dtype), 1000).T.reshape(
-            (-1, 2, 1)
-        )
-    )
-    out = UTM2geodetic(in_arr, 36, "R", WGS84.a, WGS84.b)
-    assert np.all(np.isclose(out[:, 0, 0], np.deg2rad(31.0)))
-    assert np.all(np.isclose(out[:, 1, 0], np.deg2rad(35.0)))
+# @pytest.mark.parametrize("dtype", [np.float64, np.float32])
+# def test_parallel(dtype):
+#     in_arr = np.ascontiguousarray(
+#         np.tile(np.array([[690950.46], [3431318.84]], dtype=dtype), 1000).T.reshape(
+#             (-1, 2, 1)
+#         )
+#     )
+#     out = UTM2geodetic(in_arr, 36, "R", WGS84.a, WGS84.b)
+#     assert np.all(np.isclose(out[:, 0, 0], np.deg2rad(31.0)))
+#     assert np.all(np.isclose(out[:, 1, 0], np.deg2rad(35.0)))
