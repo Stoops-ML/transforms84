3c506984075d9e3a40541331ce331f894d073d80
Daniel Stoops <dainelst@omnisys.co.il>
2025-02-24 20:45:14 +0200

    Add type parameters for NumberLikeNpy

--- src/transforms84/definitions.py
+++ src/transforms84/definitions.py
@@ -1,11 +1,11 @@
-from typing import List, Union
+from typing import Any, List, Union
 
 import numpy as np
 import numpy.typing as npt
 import pandas as pd
 
 NumberLikePy = Union[float, int]
-NumberLikeNpy = Union[np.integer, np.floating]
+NumberLikeNpy = Union[np.integer[Any], np.floating[Any]]
 NumberLike = Union[NumberLikePy, NumberLikeNpy]
 ArrayLike = Union[
     npt.NDArray[NumberLikeNpy], "pd.Series[Union[float | int]]", List[NumberLike]
